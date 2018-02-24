3cc1351b5e29c5dcbb965f457bc89f7d3d6db373
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-02-25 01:33:51 +0200

    FH-7 Add mocked NBA players

---
+++ fantasy-hoops/fantasy-hoops/ClientApp/mocked/players.js
@@ -0,0 +1,26 @@
+﻿export default [
+    {
+        id: 0,
+        name: "Lonzo",
+        surname: "Ball",
+        price: '71k',
+        position: 'PG',
+        img: 'https://nba-players.herokuapp.com/players/ball/lonzo'
+    },
+    {
+        id: 1,
+        name: "Kyrie",
+        surname: "Irving",
+        price: '86k',
+        position: 'PG',
+        img: 'https://nba-players.herokuapp.com/players/irving/kyrie'
+    },
+    {
+        id: 2,
+        name: "Jonas",
+        surname: "Valančiūnas",
+        price: '69k',
+        position: 'C',
+        img: 'https://nba-players.herokuapp.com/players/valanciunas/jonas'
+    }
+];
