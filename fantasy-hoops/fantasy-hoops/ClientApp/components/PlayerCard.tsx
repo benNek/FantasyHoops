85fdf5a5f40bb4485a9d0bf730222e47a9361345
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-02-25 01:44:40 +0200

    FH-7 Add card component

---
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/PlayerCard.tsx
@@ -0,0 +1,16 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export class PlayerCard extends React.Component<RouteComponentProps<{}>> {
+
+    public render() {
+        return <div className="card" style={{ width: '20rem' }}>
+            <img className="card-img-top" src="..." alt="Card image cap"></img>
+            <div className="card-block">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>;
+    }
+}
--- fantasy-hoops/fantasy-hoops/ClientApp/routes.tsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/routes.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Counter } from './components/Counter';
+import { PlayerCard } from './components/PlayerCard';
 
 export const routes = <div>
-    <Route exact path='/' component={ Counter } />
+    <Route exact path='/' component={Counter} />
+    <Route path='/cards/:id' component={PlayerCard} />
 </div>;
