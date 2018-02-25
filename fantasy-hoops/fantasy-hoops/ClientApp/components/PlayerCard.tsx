71ce835e5d35b9f5bef5d70238528dc7b188f96c
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-02-25 14:50:26 +0200

    FH-7 Improve card design

--- fantasy-hoops/fantasy-hoops/ClientApp/components/PlayerCard.tsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/PlayerCard.tsx
@@ -17,18 +17,21 @@ export class PlayerCard extends React.Component<any, any> {
       return <div>Sorry, but the player was not found</div>
     }
 
-    return (<div className="card" style={{ backgroundColor: `${player.teamColor}` }}>
-    
-    <div className="button">{this.buttonState()}</div>
-      <div className="player-position">{player.position}</div>
-      <div className="price-badge">
-        <span className="badge badge-dark">{player.price}</span>
+    return (
+      <div className="card" style={{ backgroundColor: `${player.teamColor}` }}>
+        <div className="ppg">{player.ppg}</div>
+        <div className="ppg ppg-label">PPG</div>
+        <div className="player-position">{player.position}</div>
+        <div className="button">{this.buttonState()}</div>
+        <div className="price-badge">
+          <span className="badge badge-dark">{player.price}</span>
+        </div>
+        <img className="card-img-top" src={player.img} alt="Card image cap"></img>
+        < div className="card-block" >
+          <h2 className="card-title">{player.name[0]}. {player.surname}</h2>
+        </div>
       </div>
-      <img className="card-img-top" src={player.img} alt="Card image cap"></img>
-      < div className="card-block" >
-        <h2 className="card-title">{player.name} {player.surname}</h2>
-      </div>
-    </div>);
+    );
   }
 
   buttonState() {
--- fantasy-hoops/fantasy-hoops/ClientApp/mocked/players.js
+++ fantasy-hoops/fantasy-hoops/ClientApp/mocked/players.js
@@ -4,27 +4,30 @@
             id: 0,
             name: 'Lonzo',
             surname: 'Ball',
-            price: '71k',
+            price: '71K',
             position: 'PG',
             teamColor: '#552583',
+            ppg: '24.7',
             img: 'https://nba-players.herokuapp.com/players/ball/lonzo'
         },
         {
             id: 1,
             name: 'Kyrie',
             surname: 'Irving',
-            price: '86k',
+            price: '86K',
             position: 'PG',
             teamColor: '#008248',
+            ppg: '31.2',
             img: 'https://nba-players.herokuapp.com/players/irving/kyrie'
         },
         {
             id: 2,
             name: 'Jonas',
             surname: 'Valančiūnas',
-            price: '69k',
+            price: '69K',
             position: 'C',
             teamColor: '#CD1141',
+            ppg: '19.8',
             img: 'https://nba-players.herokuapp.com/players/valanciunas/jonas'
         }
     ],
