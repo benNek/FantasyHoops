de8d4f122418d74987cfee62cdbc06c5488b414a
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-02-25 03:34:14 +0200

    FH-7 Able to view player card with basic PlayerAPI

--- fantasy-hoops/fantasy-hoops/ClientApp/components/PlayerCard.tsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/PlayerCard.tsx
@@ -1,15 +1,25 @@
 ﻿import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import PlayerAPI from '../mocked/players.js'
 
-export class PlayerCard extends React.Component<RouteComponentProps<{}>> {
+export class PlayerCard extends React.Component<any, any> {
 
     public render() {
-        return <div className="card" style={{ width: '20rem' }}>
-            <img className="card-img-top" src="..." alt="Card image cap"></img>
-            <div className="card-block">
-                <h4 className="card-title">Card title</h4>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+        const player = PlayerAPI.get(
+            parseInt(this.props.match.params.id)
+        )
+
+        if (!player) {
+            return <div>Sorry, but the player was not found</div>
+        }
+
+        return <div className="card" style={{ width: '20rem', backgroundColor: `${player.teamColor}` }}>
+            <img className="card-img-top" style={{maxHeight: '150px'}}
+                src={player.img} alt="Card image cap" ></img>
+            < div className="card-block" style={{ backgroundColor: 'white' }} >
+                <h2 className="card-title">{player.name} {player.surname}, {player.position}</h2>
+                <h4 className="card-text">{player.price}</h4>
+                <a href="#" className="btn btn-primary">Select</a>
             </div>
         </div>;
     }
--- fantasy-hoops/fantasy-hoops/ClientApp/mocked/players.js
+++ fantasy-hoops/fantasy-hoops/ClientApp/mocked/players.js
@@ -1,26 +1,38 @@
-﻿export default [
-    {
-        id: 0,
-        name: "Lonzo",
-        surname: "Ball",
-        price: '71k',
-        position: 'PG',
-        img: 'https://nba-players.herokuapp.com/players/ball/lonzo'
-    },
-    {
-        id: 1,
-        name: "Kyrie",
-        surname: "Irving",
-        price: '86k',
-        position: 'PG',
-        img: 'https://nba-players.herokuapp.com/players/irving/kyrie'
-    },
-    {
-        id: 2,
-        name: "Jonas",
-        surname: "Valančiūnas",
-        price: '69k',
-        position: 'C',
-        img: 'https://nba-players.herokuapp.com/players/valanciunas/jonas'
+﻿const PlayerAPI = {
+    players: [
+        {
+            id: 0,
+            name: 'Lonzo',
+            surname: 'Ball',
+            price: '71k',
+            position: 'PG',
+            teamColor: '#552583',
+            img: 'https://nba-players.herokuapp.com/players/ball/lonzo'
+        },
+        {
+            id: 1,
+            name: 'Kyrie',
+            surname: 'Irving',
+            price: '86k',
+            position: 'PG',
+            teamColor: '#008248',
+            img: 'https://nba-players.herokuapp.com/players/irving/kyrie'
+        },
+        {
+            id: 2,
+            name: 'Jonas',
+            surname: 'Valančiūnas',
+            price: '69k',
+            position: 'C',
+            teamColor: '#CD1141',
+            img: 'https://nba-players.herokuapp.com/players/valanciunas/jonas'
+        }
+    ],
+    all: function () { return this.players },
+    get: function (id) {
+        const isPlayer = p => p.id === id
+        return this.players.find(isPlayer)
     }
-];
+}
+
+export default PlayerAPI
