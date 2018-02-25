be5d6be7c610a16004c0ce47753ab3d28ac918d7
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-02-25 13:56:22 +0200

    FH-7 Update button

--- fantasy-hoops/fantasy-hoops/ClientApp/components/PlayerCard.tsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/PlayerCard.tsx
@@ -18,21 +18,25 @@ export class PlayerCard extends React.Component<any, any> {
     }
 
     return (<div className="card" style={{ backgroundColor: `${player.teamColor}` }}>
-      <img className="card-img-top" src={player.img} alt="Card image cap" ></img>
+    
+    <div className="button">{this.buttonState()}</div>
+      <div className="player-position">{player.position}</div>
+      <div className="price-badge">
+        <span className="badge badge-dark">{player.price}</span>
+      </div>
+      <img className="card-img-top" src={player.img} alt="Card image cap"></img>
       < div className="card-block" >
-        <h2 className="card-title">{player.name} {player.surname}, {player.position}</h2>
-        <h4 className="card-text">{player.price}</h4>
-        {this.buttonState()}
+        <h2 className="card-title">{player.name} {player.surname}</h2>
       </div>
     </div>);
   }
 
   buttonState() {
-    const innerHTML = this.state.selected ? <a className="glyphicon glyphicon-remove"></a> : "Select";
-    return <a className={`btn ${this.state.selected ? 'btn-danger' : 'btn-primary'} text-center`}
-            onClick={() => { this.select() }}>
-            {innerHTML}
-          </a>
+    const innerHTML = this.state.selected ? <a className="glyphicon glyphicon-remove"></a> : <a className="glyphicon glyphicon-plus"></a>;
+    return <button className={`btn btn-circle btn-lg ${this.state.selected ? 'btn-danger' : 'btn-primary'} text-center`}
+      onClick={() => { this.select() }}>
+      {innerHTML}
+    </button>
   }
 
   select() {
