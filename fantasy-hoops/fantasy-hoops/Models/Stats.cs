717cc021dd63c7ec266e68c7d8cb9f065faae9f4
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-03-06 18:43:09 +0200

    FH-56 Add Stats model

---
+++ fantasy-hoops/fantasy-hoops/Models/Stats.cs
@@ -0,0 +1,21 @@
+﻿namespace fantasy_hoops.Models
+{
+    public class GameLog
+    {
+        public int GameID { get; set; }
+        public int PTS { get; set; }
+        public int FGM { get; set; }
+        public int OREB { get; set; }
+        public int DREB { get; set; }
+        public int STL { get; set; }
+        public int AST { get; set; }
+        public int BLK { get; set; }
+        public int FGA { get; set; }
+        public int FTM { get; set; }
+        public int FTA { get; set; }
+        public int FLS { get; set; }
+        public int TOV { get; set; }
+
+        public virtual Player Player { get; set; }
+    }
+}
