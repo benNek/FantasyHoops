c03b1cbbcbd88bdeac62dc9afea399bf8169bb14
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-03-08 21:36:52 +0200

    FH-13 Another bootstrap fix...

--- fantasy-hoops/fantasy-hoops/ClientApp/components/Header.jsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/Header.jsx
@@ -7,32 +7,44 @@ export class Header extends Component {
 
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-toggleable-md navbar-dark navbar-inverse bg-primary">
-          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <a className="navbar-brand" href="#"><img src="http://www.zillakgames.com/web/game-thumbnails/C/classic%20basketball.png" width="30" height="30" alt="" /> Fantasy Hoops</a>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="#">News feed<span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Injuries feed</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Game
-                  </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <a className="dropdown-item" href="#">Lineup</a>
-                  <a className="dropdown-item" href="#">Daily Top users</a>
-                  <a className="dropdown-item" href="#">Most selected players</a>
-                </div>
-              </li>
-            </ul>
-            <ul className="nav navbar-nav ml-auto">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">
+          <img src="http://www.zillakgames.com/web/game-thumbnails/C/classic%20basketball.png" width="30" height="30" alt="" />
+          Fantasy Hoops
+        </a>
+        <button className="navbar-toggler"
+          type="button" data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">News feed<span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Injuries feed</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false">
+                Game</a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a className="dropdown-item" href="#">Lineup</a>
+                <a className="dropdown-item" href="#">Daily Top users</a>
+                <a className="dropdown-item" href="#">Most selected players</a>
+              </div>
+            </li>
+          </ul>
+          <ul className="nav navbar-nav ml-auto">
               <li className="dropdown">
                 <a className="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   Account
@@ -43,14 +55,14 @@ export class Header extends Component {
                       <div className="row">
                         <div className="col-lg-4">
                           <p className="text-center">
-                            <img src="http://www.freeiconspng.com/uploads/profile-icon-9.png" width="100" height="120" alt="" />
+                            <img src="https://i.imgur.com/0i1KEYY.png" width="100" height="100" alt="" />
                           </p>
                         </div>
                         <div className="col-lg-8">
-                          <p className="text-left"><strong>Nombre Apellido</strong></p>
-                          <p className="text-left small">correoElectronico@email.com</p>
+                          <p className="text-left"><strong>hooper420</strong></p>
+                          <p className="text-left small">fantasy@hoops.com</p>
                           <p className="text-left">
-                            <a href="#" className="btn btn-primary btn-block btn-sm">Actualizar Datos</a>
+                            <a href="#" className="btn btn-primary btn-block btn-sm">Edit profile</a>
                           </p>
                         </div>
                       </div>
@@ -62,7 +74,7 @@ export class Header extends Component {
                       <div className="row">
                         <div className="col-lg-12">
                           <p>
-                            <a href="#" className="btn btn-danger btn-block">Cerrar Sesion</a>
+                            <a href="#" className="btn btn-danger btn-block">Logout</a>
                           </p>
                         </div>
                       </div>
@@ -71,9 +83,8 @@ export class Header extends Component {
                 </ul>
               </li>
             </ul>
-          </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     );
   }
 }
