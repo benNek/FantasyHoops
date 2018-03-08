97baf56386d15c969da1c2421b2fd4da70966e01
Paulius <p.prakapavicius@gmail.com>
2018-03-08 15:15:14 +0200

    Add User Model

---
+++ fantasy-hoops/fantasy-hoops/Migrations/20180308131226_AddUsersModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace fantasy_hoops.Migrations
+{
+    public partial class AddUsersModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: false),
+                    FavoriteTeam = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: false),
+                    Phone = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
