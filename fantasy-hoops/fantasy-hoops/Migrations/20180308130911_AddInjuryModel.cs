bb291b4eaee12bb6ef9b4624b04565d0c364eed3
Vilius <viliussapauskas2g2@gmail.com>
2018-03-08 15:11:33 +0200

    FH-61 Create injuries model

---
+++ fantasy-hoops/fantasy-hoops/Migrations/20180308130911_AddInjuryModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace fantasy_hoops.Migrations
+{
+    public partial class AddInjuryModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Injuries",
+                columns: table => new
+                {
+                    InjuryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Injury = table.Column<string>(nullable: true),
+                    Link = table.Column<string>(nullable: true),
+                    PlayerID = table.Column<int>(nullable: false),
+                    Status = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Injuries", x => x.InjuryID);
+                    table.ForeignKey(
+                        name: "FK_Injuries_Players_PlayerID",
+                        column: x => x.PlayerID,
+                        principalTable: "Players",
+                        principalColumn: "PlayerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Injuries_PlayerID",
+                table: "Injuries",
+                column: "PlayerID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Injuries");
+        }
+    }
+}
