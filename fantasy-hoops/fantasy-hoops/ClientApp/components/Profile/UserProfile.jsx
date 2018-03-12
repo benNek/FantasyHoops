23417700a30471e78d88834ac6ec0eaea3c230c9
Paulius <p.prakapavicius@gmail.com>
2018-03-12 19:33:45 +0200

    FH-26 Fix edit profile

--- fantasy-hoops/fantasy-hoops/ClientApp/components/Profile/UserProfile.jsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/Profile/UserProfile.jsx
@@ -46,7 +46,7 @@ export class UserProfile extends Component {
                       <a href="#" className="badge badge-dark badge-pill" style={{ backgroundColor: '#008248' }} >Boston Celtics</a>
                     </div>
                     <hr />
-                    <span className="badge badge-primary"><i className="fa fa-user"></i> Streak</span>
+                    <span className="badge badge-primary"><i className="fa fa-ban"></i> Streak</span>
                     <span className="badge badge-success"><i className="fa fa-cog"></i> Total Score</span>
                     <span className="badge badge-danger"><i className="fa fa-eye"></i> Ranking</span>
                   </div>
@@ -134,63 +134,21 @@ export class UserProfile extends Component {
                     </div>
                   </div>
                   <div className="form-group row">
-                    <label className="col-lg-3 col-form-label form-control-label">Last name</label>
-                    <div className="col-lg-9">
-                      <input className="form-control" type="text" value="Bishop" />
-                    </div>
-                  </div>
-                  <div className="form-group row">
                     <label className="col-lg-3 col-form-label form-control-label">Email</label>
                     <div className="col-lg-9">
                       <input className="form-control" type="email" value="email@gmail.com" />
                     </div>
                   </div>
                   <div className="form-group row">
-                    <label className="col-lg-3 col-form-label form-control-label">Company</label>
-                    <div className="col-lg-9">
-                      <input className="form-control" type="text" value="" />
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-lg-3 col-form-label form-control-label">Website</label>
+                    <label className="col-lg-3 col-form-label form-control-label">About me</label>
                     <div className="col-lg-9">
                       <input className="form-control" type="url" value="" />
                     </div>
                   </div>
                   <div className="form-group row">
-                    <label className="col-lg-3 col-form-label form-control-label">Address</label>
+                    <label className="col-lg-3 col-form-label form-control-label">Favorite team</label>
                     <div className="col-lg-9">
-                      <input className="form-control" type="text" value="" placeholder="Street" />
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-lg-3 col-form-label form-control-label"></label>
-                    <div className="col-lg-6">
-                      <input className="form-control" type="text" value="" placeholder="City" />
-                    </div>
-                    <div className="col-lg-3">
-                      <input className="form-control" type="text" value="" placeholder="State" />
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-lg-3 col-form-label form-control-label">Time Zone</label>
-                    <div className="col-lg-9">
-                      <select id="user_time_zone" className="form-control" size="0">
-                        <option value="Hawaii">(GMT-10:00) Hawaii</option>
-                        <option value="Alaska">(GMT-09:00) Alaska</option>
-                        <option value="Pacific Time (US &amp; Canada)">(GMT-08:00) Pacific Time (US &amp; Canada)</option>
-                        <option value="Arizona">(GMT-07:00) Arizona</option>
-                        <option value="Mountain Time (US &amp; Canada)">(GMT-07:00) Mountain Time (US &amp; Canada)</option>
-                        <option value="Central Time (US &amp; Canada)" value="selected">(GMT-06:00) Central Time (US &amp; Canada)</option>
-                        <option value="Eastern Time (US &amp; Canada)">(GMT-05:00) Eastern Time (US &amp; Canada)</option>
-                        <option value="Indiana (East)">(GMT-05:00) Indiana (East)</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-lg-3 col-form-label form-control-label">Username</label>
-                    <div className="col-lg-9">
-                      <input className="form-control" type="text" value="janeuser" />
+                      <input className="form-control" type="url" value="" />
                     </div>
                   </div>
                   <div className="form-group row">
