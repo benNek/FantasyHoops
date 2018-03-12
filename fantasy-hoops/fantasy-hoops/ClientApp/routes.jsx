497e4b8f3d2738619f7e78aebf91836265b06ce1
Paulius <p.prakapavicius@gmail.com>
2018-03-12 12:52:43 +0200

    FH-26 Add user profile template

--- fantasy-hoops/fantasy-hoops/ClientApp/routes.jsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/routes.jsx
@@ -3,8 +3,10 @@ import { Route } from 'react-router-dom';
 import { Lineup } from './components/Lineup/Lineup';
 import { Header } from './components/Header';
 import { InjuriesFeed } from './components/Injuries/InjuriesFeed';
+import { UserProfile } from './components/UserProfile';
 
 export const routes = <div>
+    <Route path='/profile' component={UserProfile} />
     <Route path='/' component={Header} />
     <Route path='/lineup' component={Lineup} />
     <Route path='/injuries' component={InjuriesFeed} />
