8214cad2c0fc430ee7988f64a4b369d9f31e0a47
donciakss <donciakss@users.noreply.github.com>
2018-03-13 23:14:58 +0200

    FH-27 Add spaces between cards

--- fantasy-hoops/fantasy-hoops/ClientApp/components/News/NewsCard.jsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/News/NewsCard.jsx
@@ -12,13 +12,13 @@ export class NewsCard extends Component {
     const cutPosition = 300;
     return (
 
-  <div className="mx-auto news-card card">
+      <div className="mt-5 mb-5 mx-auto news-card card">
         <div className="card-header bg-info"><h5 className="card-title">{this.props.news.title}</h5></div>
         <div className="card-body news-title">
           <div>
             <input onChange={this.handleCheck} checked={this.state.checked} type="checkbox" className="read-more-state" id={this.props.news.id} />
             <p className="read-more-wrap">{text.substring(0, cutPosition)}{!this.state.checked ? '...' : ''}<span className="read-more-target">{text.substring(cutPosition, text.length)}</span></p>
-            <label htmlFor={this.props.news.id}  className="read-more-trigger"></label>
+            <label htmlFor={this.props.news.id} className="read-more-trigger"></label>
           </div>
         </div>
         <div className="card-footer text-muted mt-2">
--- fantasy-hoops/fantasy-hoops/ClientApp/components/News/NewsFeed.jsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/News/NewsFeed.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { NewsCard } from './NewsCard';
 import NewsAPI from '../../mocked/News';
 import _ from 'lodash'
+import shortid from 'shortid';
 
 export class NewsFeed extends Component {
   constructor(props) {
@@ -10,14 +11,14 @@ export class NewsFeed extends Component {
   render() {
     const news = _.map(NewsAPI.all(),
       (news) => {
-        return <NewsCard news={news} />
+        return <NewsCard key={shortid()} news={news} />
       }
     );
     return (
       <div className="container mt-4 bg-light" style={{ paddingTop: '3em' }}>
         <div className="center col">
           {news}
-        </div>   
+        </div>
       </div>
     );
   }
