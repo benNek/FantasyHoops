d6f419e74af94fe2d455004643f4cb2762b5d4cc
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-03-14 00:10:05 +0200

    FH-27 Add Teams' logos

--- fantasy-hoops/fantasy-hoops/ClientApp/components/News/NewsCard.jsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/News/NewsCard.jsx
@@ -7,25 +7,59 @@ export class NewsCard extends Component {
     }
     this.handleCheck = this.handleCheck.bind(this);
   }
+
   render() {
     const text = this.props.news.news;
     const cutPosition = 300;
     return (
-
-      <div className="mt-5 mb-5 mx-auto news-card card">
-        <div className="card-header bg-info"><h5 className="card-title">{this.props.news.title}</h5></div>
-        <div className="card-body news-title">
-          <div>
-            <input onChange={this.handleCheck} checked={this.state.checked} type="checkbox" className="read-more-state" id={this.props.news.id} />
-            <p className="read-more-wrap">{text.substring(0, cutPosition)}{!this.state.checked ? '...' : ''}<span className="read-more-target">{text.substring(cutPosition, text.length)}</span></p>
-            <label htmlFor={this.props.news.id} className="read-more-trigger"></label>
-          </div>
+      <div className="mt-5 mx-auto news-card card">
+        <div className="card-header bg-info text-white">
+          <h5 className="card-title" style={{ marginBottom: '0' }}>
+            {this.props.news.title}
+          </h5>
         </div>
-        <div className="card-footer text-muted mt-2">
-          {this.props.news.date}</div>
+        <span>
+          <img
+            src="http://i.cdn.turner.com/nba/nba/assets/logos/teams/secondary/web/PHI.svg"
+            alt=""
+            width="50px"
+            style={{ position: 'absolute' }}
+          />
+        </span>
+        <span style={{ paddingLeft: '4rem' }}>
+          <img
+            src="http://i.cdn.turner.com/nba/nba/assets/logos/teams/secondary/web/CHA.svg"
+            alt=""
+            width="50px"
+            style={{ position: 'absolute' }}
+          />
+        </span>
+        <div className="card-header text-muted" style={{ height: '3rem', paddingLeft: '3rem' }}>
+          vs.
+          <span style={{ float: 'right' }}>
+            {this.props.news.date}
+          </span>
+        </div>
+        <div className="card-body">
+          <input
+            onChange={this.handleCheck}
+            checked={this.state.checked}
+            type="checkbox"
+            className="read-more-state"
+            id={this.props.news.id}
+          />
+          <p className="read-more-wrap" style={{textAlign: 'justify'}}>{text.substring(0, cutPosition)}
+            {!this.state.checked ? '...' : ''}
+            <span className="read-more-target">
+              {text.substring(cutPosition, text.length)}
+            </span>
+          </p>
+          <label htmlFor={this.props.news.id} className="read-more-trigger"></label>
+        </div>
       </div>
     );
   }
+
   handleCheck() {
     this.setState({
       checked: !this.state.checked
