b3b5120485167524130b951588f45da778f15350
Benas Nekrošius <benasnek@gmail.com>
2018-03-28 00:42:44 +0300

    FH-62 Create News Controller

---
+++ fantasy-hoops/fantasy-hoops/Controllers/NewsController.cs
@@ -0,0 +1,34 @@
+﻿using fantasy_hoops.Database;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fantasy_hoops.Controllers
+{
+    [Route("api/[controller]")]
+    public class NewsController
+    {
+
+        private readonly GameContext context;
+
+        public NewsController()
+        {
+            context = new GameContext();
+        }
+
+        [HttpGet]
+        public IEnumerable<Object> Get()
+        {
+            
+            return context.News.Select(x => new
+            {
+                id = x.NewsID,
+                x.Title,
+                news = String.Join(String.Empty, x.Paragraphs.Select(y => y.Content).ToArray()),
+                x.Date
+            }).ToList();
+        }
+    }
+}
