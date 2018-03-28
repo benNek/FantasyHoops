91981527a19cd76bc37700bbb430af911df34352
Benas Nekrošius <benasnek@gmail.com>
2018-03-29 01:59:50 +0300

    FH-76 Add infinite scrolling

--- fantasy-hoops/fantasy-hoops/ClientApp/components/News/NewsFeed.jsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/News/NewsFeed.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { NewsCard } from './NewsCard';
 import _ from 'lodash'
 import shortid from 'shortid';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class NewsFeed extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      news: ''
+      news: '',
+      hasMore: true,
     }
+    this.fetchData = this.fetchData.bind(this);
   }
 
   componentDidMount() {
@@ -23,6 +26,19 @@ export class NewsFeed extends Component {
       });
   }
 
+  fetchData() {
+    fetch(`http://localhost:51407/api/news?start=${this.state.news.length}`)
+      .then(res => {
+        return res.json()
+      })
+      .then(res => {
+        this.setState({
+          news: this.state.news.concat(res),
+          hasMore: res.length == 6
+        });
+      });
+  }
+
   render() {
     let news = _.map(this.state.news,
       (news) => {
@@ -32,7 +48,14 @@ export class NewsFeed extends Component {
     return (
       <div className="container bg-light pt-5 pb-1">
         <div className="center col">
-          {news}
+          <InfiniteScroll
+            dataLength={news.length}
+            next={this.fetchData}
+            hasMore={this.state.hasMore}
+            loader={<h4>Loading...</h4>}
+            >
+            {news}
+          </InfiniteScroll>
         </div>
       </div>
     );
