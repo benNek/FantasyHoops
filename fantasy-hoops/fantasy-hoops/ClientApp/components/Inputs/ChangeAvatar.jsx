5f795f841778344b266cca6b9f6da1fb6ff2b4ed
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-04-15 16:39:22 +0300

    FH-87 Disabling buttons

--- fantasy-hoops/fantasy-hoops/ClientApp/components/Inputs/ChangeAvatar.jsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/Inputs/ChangeAvatar.jsx
@@ -7,10 +7,14 @@ import { Alert } from '../Alert';
 export class ChangeAvatar extends Component {
   constructor(props) {
     super(props);
+
     this.onCrop = this.onCrop.bind(this);
     this.onClose = this.onClose.bind(this);
+    this.closeImage = this.closeImage.bind(this);
+    this.loadImage = this.loadImage.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleClear = this.handleClear.bind(this);
+
     this.state = {
       preview: null,
       showAlert: false,
@@ -19,9 +23,27 @@ export class ChangeAvatar extends Component {
     }
   }
 
-  onClose() {
+  componentDidMount() {
+    const saveBtn = document.getElementById('save');
+    saveBtn.disabled = false;
+  }
+
+  closeImage() {
     this.setState({
       preview: null,
+      showAlert: false,
+      alertType: '',
+      alertText: ''
+    });
+  }
+
+  loadImage() {
+    const saveBtn = document.getElementById('save');
+    saveBtn.disabled = false;
+  }
+
+  onClose() {
+    this.setState({
       showAlert: false,
       alertType: '',
       alertText: ''
@@ -66,6 +88,8 @@ export class ChangeAvatar extends Component {
   }
 
   handleClear() {
+    const saveBtn = document.getElementById('save');
+    saveBtn.disabled = true;
     const user = parse();
     const data = {
       id: user.id
@@ -96,6 +120,12 @@ export class ChangeAvatar extends Component {
   }
 
   render() {
+    const clearAvatar = <div>
+      <div className="text-center mt-2 mb-2">or</div>
+      <div className="text-center">
+        <button onClick={this.handleClear} type="button" className="btn btn-outline-danger">Clear avatar</button>
+      </div>
+    </div>;
     return (
       <div className="modal fade" id="changeImage" tabIndex="-1" role="dialog" aria-labelledby="changeImage" aria-hidden="true">
         <div className="modal-dialog" role="document">
@@ -112,15 +142,13 @@ export class ChangeAvatar extends Component {
                 width={350}
                 height={250}
                 onCrop={this.onCrop}
-                onClose={this.onClose}
+                onClose={this.closeImage}
+                onFileLoad={this.loadImage}
               />
-              <div className="text-center mt-2 mb-2">or</div>
-              <div className="text-center">
-                <button onClick={this.handleClear} type="button" className="btn btn-outline-danger">Clear avatar</button>
-              </div>
+              {!this.state.preview ? clearAvatar : ''}
             </div>
             <div className="modal-footer mx-auto">
-              <button onClick={this.handleSubmit} type="button" className="btn btn-primary">Save changes</button>
+              <button disabled={false} id="save" onClick={this.handleSubmit} type="button" className="btn btn-primary">Save changes</button>
               <button onClick={this.onClose} type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
             </div>
           </div>
