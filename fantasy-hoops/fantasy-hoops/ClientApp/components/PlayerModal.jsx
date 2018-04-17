06d2491b0e265416609ed1e62f661eeca4c02a05
Benas Nekrošius <benasnek@gmail.com>
2018-04-17 11:28:17 +0300

    FH-40 Add points chart

--- fantasy-hoops/fantasy-hoops/ClientApp/components/PlayerModal.jsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/PlayerModal.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
 import shortid from 'shortid';
 import moment from 'moment';
 import _ from 'lodash';
@@ -12,7 +13,9 @@ export class PlayerModal extends Component {
     let stats = '';
     let teamLogo = '';
     let rows = '';
+    let chartData = this.getChartData('null', 'null');
     if (this.props.stats) {
+      chartData = this.getChartData('Points', 'pts');
       stats = this.props.stats;
       teamLogo = `http://i.cdn.turner.com/nba/nba/assets/logos/teams/secondary/web/${stats.team.abbreviation}.svg`;
       rows = _.map(stats.games, (s) => {
@@ -73,6 +76,7 @@ export class PlayerModal extends Component {
                 <div className="nav nav-tabs justify-content-end" id="nav-tab" role="tablist" style={{ position: 'static' }}>
                   <a className="nav-item nav-link active btn-no-otline" id="nav-stats-tab" data-toggle="tab" href="#nav-stats" role="tab" aria-controls="nav-stats" aria-selected="true">Season stats</a>
                   <a className="nav-item nav-link btn-no-otline" id="nav-gamelog-tab" data-toggle="tab" href="#nav-gamelog" role="tab" aria-controls="nav-gamelog" aria-selected="false">Gamelog</a>
+                  <a className="nav-item nav-link btn-no-otline" id="nav-charts-tab" data-toggle="tab" href="#nav-charts" role="tab" aria-controls="nav-charts" aria-selected="false">Charts</a>
                 </div>
               </nav>
               <div className="tab-content" id="nav-tabContent">
@@ -137,11 +141,68 @@ export class PlayerModal extends Component {
                     </tbody>
                   </table>
                 </div>
+                <div className="tab-pane fade" id="nav-charts" role="tabpanel" aria-labelledby="nav-charts-tab">
+                  <Line data={chartData} />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     );
+  }
+
+  getChartData(title, criteria) {
+    if (title == 'null') {
+      const data = {
+        labels: labels,
+        datasets: [
+          {
+            label: title
+          }
+        ]
+      }
+      return data;
+    }
+    let labels = [];
+    let values = [];
+
+    const games = this.props.stats.games;
+    games.sort(function (a, b) {
+      return new Date(a.date) - new Date(b.date);
+    });
+
+    games.forEach(game => {
+      labels.push(game.date.substring(5, 10));
+      values.push(game[criteria]);
+    });
+
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: title,
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: this.props.stats.team.color,
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: values
+        }
+      ]
+    };
+    return data;
   }
 }
