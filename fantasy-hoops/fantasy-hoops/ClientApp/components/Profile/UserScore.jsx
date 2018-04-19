fb7ddc916af77a4a8bb4f19bbd667178c21cbe6f
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-04-19 11:18:49 +0300

    FH-8 Rebase

--- fantasy-hoops/fantasy-hoops/ClientApp/components/Profile/UserScore.jsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/Profile/UserScore.jsx
@@ -23,12 +23,13 @@ export class UserScore extends Component {
       <div className="card bg-white rounded mt-2 user-score">
         <div className="card-body">
           <div className="row">
-          {players}
-        </div>
-        <div className="card-text user-score-total text-center">
-          <h4>{this.props.activity.score}</h4>
-          <h6 style={{ marginTop: "-0.7rem" }}>PTS</h6>
-          <h6 style={{ fontSize: "0.8rem", marginTop: "1.6rem", fontWeight: 400 }}>{this.props.activity.date.substring(0, 10)}</h6>
+            {players}
+            <div className="pl-5 pt-3">
+              <h4>{this.props.activity.score}</h4>
+              <h6 className="text-center pr-2" style={{ marginTop: "-0.7rem" }}>PTS</h6>
+              <h6 style={{ fontSize: "0.8rem", marginTop: "0.5rem", fontWeight: 400 }}>{this.props.activity.date.substring(0, 10)}</h6>
+            </div>
+          </div>
         </div>
       </div>
     );
