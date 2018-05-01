4235cd38234ba9906d85c962c34447dca5d37663
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-05-02 01:12:22 +0300

    FH-97 Update Player model

---
+++ fantasy-hoops/fantasy-hoops/Migrations/20180501220922_AddStatusColumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace fantasy_hoops.Migrations
+{
+    public partial class AddStatusColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Players",
+                nullable: false,
+                defaultValue: "Active");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Players");
+        }
+    }
+}
