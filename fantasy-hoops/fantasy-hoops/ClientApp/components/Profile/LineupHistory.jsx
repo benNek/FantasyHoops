76a590c1250a3f9e91d991b5db81a59899b8db82
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-05-15 15:41:12 +0300

    FH-106 See more button

--- fantasy-hoops/fantasy-hoops/ClientApp/components/Profile/LineupHistory.jsx
+++ fantasy-hoops/fantasy-hoops/ClientApp/components/Profile/LineupHistory.jsx
@@ -6,13 +6,17 @@ import { UserScore } from './UserScore';
 import icon from '../../content/basketball-player-scoring.svg';
 import { parse } from '../../utils/auth';
 const user = parse();
+const LOAD_COUNT = 5;
 
 export class LineupHistory extends Component {
   constructor(props) {
     super(props);
+    this.loadMore = this.loadMore.bind(this);
 
     this.state = {
+      loadCounter: 0,
       user: user,
+      history: [],
       loader: true,
       posIMG: this.getPosImages(),
       playerIMG: this.getPlayerImages()
@@ -20,47 +24,63 @@ export class LineupHistory extends Component {
   }
 
   async componentWillMount() {
-    await fetch(`http://localhost:51407/api/user/${user.id}`)
+    await fetch(`http://localhost:51407/api/user/${user.id}?count=10`)
       .then(res => res.json())
       .then(res => {
         this.setState({
-          user: res,
+          history: res.history,
           loader: false,
           readOnly: false
         });
       });
   }
 
+  async loadMore() {
+    this.setState({
+      loadCounter: this.state.loadCounter + 1,
+      loader: true
+    });
+    await fetch(`http://localhost:51407/api/user/${user.id}?start=${this.state.history.length}&count=${LOAD_COUNT}`)
+      .then(res => {
+        return res.json()
+      })
+      .then(res => {
+        this.setState({
+          history: this.state.history.concat(res.history),
+          loader: false
+        });
+      });
+  }
+
   render() {
-    const history = () => {
-      if (!this.state.loader) {
-        return _.map(
-          this.state.user.history,
-          (activity) => {
-            return (
-              <UserScore
-                key={shortid()}
-                posIMG={this.state.posIMG}
-                playerIMG={this.state.playerIMG}
-                activity={activity}
-                showModal={this.showModal}
-              />
-            )
-          });
-      } else {
-        return '';
-      }
-    }
+    const history = _.map(
+      this.state.history,
+      (activity) => {
+        return (
+          <UserScore
+            key={shortid()}
+            posIMG={this.state.posIMG}
+            playerIMG={this.state.playerIMG}
+            activity={activity}
+            showModal={this.showModal}
+          />
+        )
+      });
+
+    const btn = this.state.loadCounter * LOAD_COUNT + 10 > this.state.history.length
+      ? ''
+      : <button className="btn btn-primary m-3" onClick={this.loadMore}>See more</button>;
 
     return (
       <div className="container bg-light p-4">
-        <h3 className="text-center"><span><img src={icon} width="65rem" /></span> User lineup history</h3>
-        <div className="m-3">
-        </div>
+        <h3 className="text-center"><span><img src={icon} width="65rem" /></span> Your lineup history</h3>
         <div className="center">
-          {history()}
+          {history}
+          <Loader show={this.state.loader} />
         </div>
-        <Loader show={this.state.loader} />
+        <div className="text-center">
+          {!this.state.loader ? btn : ''}
+        </div>
       </div>
     );
   }
